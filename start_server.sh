9df4eb7e54d16f83fdd127fe527df7ff00f55a3c
Ari Morales <7206453+garissman@users.noreply.github.com>
2025-11-09 17:59:57 -0600

    code update

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+git pull
 /home/james/.nvm/versions/node/v22.21.0/bin/npm --prefix /home/james/jamesProject/frontend install
 /home/james/.nvm/versions/node/v22.21.0/bin/npm --prefix /home/james/jamesProject/frontend run build
 /home/james/jamesProject/venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000
