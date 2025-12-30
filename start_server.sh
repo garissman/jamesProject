d719c0299d7a7a86d9a78a78775209446fd087ae
Ari Morales <ari.morales@travelwifi.com>
2025-12-29 22:19:43 -0600

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,4 @@
 git pull
 /home/james/.nvm/versions/node/v22.21.0/bin/npm --prefix /home/james/jamesProject/frontend install
 /home/james/.nvm/versions/node/v22.21.0/bin/npm --prefix /home/james/jamesProject/frontend run build
-/home/james/jamesProject/venv/bin/uvicorn main:app --port 8000
+/home/james/jamesProject/venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000
